0bd34c8e0fa19db7cd438e1ef7e5bcd27286b32d
Edouard Dupin <yui.heero@gmail.com>
2012-03-08 17:44:56 +0100

    generate difference in the compilation in debug ant compilation realease

--- jni/Linux.mk
+++ jni/Linux.mk
@@ -12,9 +12,15 @@ include $(LOCAL_PATH)/file.mk
 LOCAL_SRC_FILES := $(FILE_LIST)
 LOCAL_LDLIBS    := 
 
-LOCAL_CFLAGS    :=	-D__PLATFORM__Linux \
+ifeq ($(DEBUG),1)
+LOCAL_CFLAGS    :=  -D__PLATFORM__Linux \
                     -DEWOL_USE_FREE_TYPE \
                     -DDRAW_DEBUG_LEVEL=3
+else
+LOCAL_CFLAGS    :=  -D__PLATFORM__Linux \
+                    -DEWOL_USE_FREE_TYPE \
+                    -DDRAW_DEBUG_LEVEL=1
+endif
 
 include $(BUILD_EXECUTABLE)
 
